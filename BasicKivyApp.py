c4b82417f0cf740c4633cca7858659ff4fe61b01
TomatoSoupM8 <joseguombinho@gmail.com>
2016-10-09 12:09:25 +0200

    BasicKivyApp.py
    It's basic kivy application.

---
+++ BasicKivyApp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'__author__: TomatoSoupM8'
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class SimpleKivy(App):
+	def build(self):
+		return Label(text="Hello World")
+		
+if __name__ == '__main__':
+	SimpleKivy().run()
